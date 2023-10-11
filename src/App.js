b0b56f5d2258043806f20a82d023067addb40b91
Md-Asim786 <mdasim2004@gmail.com>
2023-10-11 19:49:40 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename='/my-portfolio'>
         <Routes>
-          <Route path="/" element={<Navigation />}>
+          <Route path="/my-portfolio" element={<Navigation />}>
             <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="services" element={<Services />} />
-            <Route path="skills" element={<Skills />} />
-            <Route path="portfolio" element={<Portfolio />} />
-            <Route path="contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
